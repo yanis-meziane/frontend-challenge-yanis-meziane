5dc81459cd2335b03046d5f9d05b40d06e851f4f
Yanis Meziane <yanismeziane04@icloud.com>
2025-07-31 18:20:15 +0200

    avancées

--- src/GameChoice.tsx
+++ src/GameChoice.tsx
@@ -3,9 +3,41 @@ import "../src/CSS/GameChoice.css"
 
 const GameChoice = () => {
  return(
-    <div id="type">
-        <h1>Bonjour</h1>
+    <>
+        <div id="type">
+            <h3>Bonjour</h3>
+            <p>Sélectionner parmi 4 jeux interactifs pour engager vos utilisateurs et créer une expérience unique</p>
+        </div>
+        
+    <div id="jeux">
+        <section className="nav-links">
+          <article>
+            <button id="firstgame">
+                Code PIN
+            </button>
+          </article>
+
+          <article>
+            <button id="secondGame">
+              QR Code
+            </button>
+          </article>
+
+          <article>
+            <button id="thirdGame">
+              Sauvergarder
+            </button>
+          </article>
+
+          <article>
+                <button id="fourtGame">
+                    hello
+                </button>
+          </article>
+        </section>
     </div>
+</>
+
  )   
 }
 
