5588ce7f3f9a866b714fd096578498cfbf8fd86e
Yanis Meziane <yanismeziane04@icloud.com>
2025-08-01 11:26:03 +0200

    Avancée sur la partie personnalisation

---
+++ src/personalizeGame/personalize.tsx
@@ -0,0 +1,16 @@
+import "../CSS/personalize.css"
+
+const Personalize = () => {
+    return(
+       <>
+            <div id="type">
+                <h3>Personnaliser votre jeu</h3>
+                <p>importer votre logo et sélectionner vos couleurs pour créer un jeu à l'image de votre marque.</p>
+                <p>Offrez à vos clients une expérience unique, entièrement personnalisée.</p>
+            </div>
+       </> 
+    )
+}
+
+
+export default Personalize
